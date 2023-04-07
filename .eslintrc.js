6323c06f44e9bd4c4bfb0563ef17e09742daf9c5
Anton Pryadkin <ap.pryadkin@dm-solutions.ru>
2023-04-07 13:26:08 +0600

    update eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +20,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    // Enforce consistent spacing inside braces https://eslint.org/docs/rules/object-curly-spacing
+    "object-curly-spacing": "off",
+    '@typescript-eslint/object-curly-spacing': ['error', 'never'],
   },
 };
