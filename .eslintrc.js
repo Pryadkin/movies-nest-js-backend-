75ef6ae616a6a0c8ea1e192411c606d56f30f33c
Anton Pryadkin <ap.pryadkin@dm-solutions.ru>
2023-04-08 11:04:39 +0600

    add jwt.config and mongo.config, update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,9 +22,11 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
 
     // Enforce consistent spacing inside braces https://eslint.org/docs/rules/object-curly-spacing
-    "object-curly-spacing": "off",
+    'object-curly-spacing': 'off',
     '@typescript-eslint/object-curly-spacing': ['error', 'never'],
 
-    'no-console': 1
+    'no-console': 1,
+
+    'quotes': ["error", "single"]
   },
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import {Module} from '@nestjs/common'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
+import {ConfigModule, ConfigService} from '@nestjs/config'
+import {TypegooseModule} from 'nestjs-typegoose'
+import {getMongoDbConfig} from './config/mongo.config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoDbConfig,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
